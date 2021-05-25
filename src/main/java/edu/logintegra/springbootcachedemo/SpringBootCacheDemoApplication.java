e01529d8c64b39641b67ddd28e5ab65539e98a1a
Paweł Staniszewski <pawel.staniszewski@logintegra.com>
2021-05-26 01:15:08 +0200

    Szablon projektu Spring Boot

---
+++ src/main/java/edu/logintegra/springbootcachedemo/SpringBootCacheDemoApplication.java
@@ -0,0 +1,13 @@
+package edu.logintegra.springbootcachedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCacheDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCacheDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/logintegra/springbootcachedemo/SpringBootCacheDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.logintegra.springbootcachedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCacheDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
