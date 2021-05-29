e4d85511f90a4a8dddfda0c9eaa8bfae467c9c7f
Paweł Staniszewski <pawel.staniszewski@logintegra.com>
2021-05-30 00:55:32 +0200

    Podstawowy przykład użycia Redis jako bazy danych
    Poprawia konfigurację Redis dla `RedisConnectionFactory`.

--- src/main/java/edu/logintegra/springbootcachedemo/config/RedisConfig.java
+++ src/main/java/edu/logintegra/springbootcachedemo/config/RedisConfig.java
@@ -1,22 +1,44 @@
 package edu.logintegra.springbootcachedemo.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 /**
- * https://docs.spring.io/spring-data/data-redis/docs/current/reference/html/#redis.repositories
+ * Na podstawie:
+ *
+ * <ul>
+ *     <li>https://docs.spring.io/spring-data/data-redis/docs/current/reference/html/#redis.repositories</li>
+ *     <li>https://stackoverflow.com/a/49032627/2224598</li>
+ * </ul>
  */
 @Configuration
 @EnableRedisRepositories
+@Slf4j
 public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.redis.database}")
+    private int redisDatabase;
 
     @Bean
     public RedisConnectionFactory connectionFactory() {
-        return new JedisConnectionFactory();
+        log.info("Konfigurujemy Redis -- baza danych: " + redisDatabase + ", port: " + redisPort);
+
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
+        redisStandaloneConfiguration.setDatabase(redisDatabase);
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
